e7d1d5afd3b74bfe940864fdbc3936c54ccb3fbd
Enterprise09 <goldenstaytes@gmail.com>
2021-08-20 01:54:25 +0900

    design update displayName form & logout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Profile.css";
 
-const Profile = () => {
-  return <h1>This page should be update profile</h1>;
+const Profile = ({ userObj }) => {
+  const [newUserName, setNewUserName] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewUserName(value);
+  };
+
+  const onSubmit = () => {};
+
+  const onLogoutClick = () => {};
+  return (
+    <div className="profile_container">
+      <form className="profile_update_form" onSubmit={onSubmit}>
+        <input
+          className="profile_updateText"
+          onChange={onChange}
+          type="text"
+          value={newUserName}
+          placeholder="Display Name"
+        />
+        <input className="profile_updateBtn" type="submit" value="Update" />
+      </form>
+      <hr />
+      <button
+        className="profile_logoutBtn"
+        onClick={onLogoutClick}
+        type="button"
+      >
+        LogOut
+      </button>
+    </div>
+  );
 };
 
 export default Profile;
