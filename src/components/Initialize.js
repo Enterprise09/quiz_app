2b4c862749d1680ac57efc22f4c58ce2d4240bb2
Enterprise09 <goldenstaytes@gmail.com>
2021-08-29 01:19:18 +0900

    design init page

--- src/components/Initialize.js
+++ src/components/Initialize.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Initialize = () => {
-  return <h1 style={{ color: "grey", margin: "auto" }}>Initialize . . .</h1>;
+  return (
+    <>
+      <div
+        style={{
+          alignSelf: "center",
+          marginTop: "200px",
+        }}
+      >
+        <img
+          src="https://blog.kakaocdn.net/dn/8afmG/btqB11CYpEu/ho6hGIigdK9Fm3e7p9XTQ0/img.gif"
+          style={{
+            borderRadius: "70%",
+            maxWidth: "520px",
+            width: "100%",
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Initialize;
